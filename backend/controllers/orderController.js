ca720f36ffc126b4ef8f5f4ba60fdb840510a332
Ramanarao21 <ramanaraoVK18@gmail.com>
2025-04-04 08:49:56 +0530

    added frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,8 +8,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-    // const frontend_url = "https://food-del-645679.netlify.app";
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://order-food-5cc1dd.netlify.app/";
+    // const frontend_url = "http://localhost:5173";
 
     try {
 
